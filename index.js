878322b7fcbb0d7a10d40b6d75938ba67227de03
Dxuios <74361015+dxuios@users.noreply.github.com>
2024-12-02 11:23:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function appendToDisplay(value) {
+   
+  document.getElementById('display').value += value;
+}
+
+
+function clearDisplay(){
+  document.getElementById('display').value = '';
+}
+function calculate() {
+
+  const expression = document.getElementById('display').value;
+
+  try {
+    const result = eval(expression);
+    document.getElementById('display').value = result;
+  } catch (error) {
+    document.getElementById('display').value = 'Ошибка';
+  }
+}
